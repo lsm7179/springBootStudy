4eb38cd79acb24ca67c257d3098f1bc815cf9fbe
이승민 <sml7179@naver.com>
2021-06-04 17:39:27 +0900

    타임리프 템플릿에 값 전달하기

--- src/main/java/com/example/springstudy/controller/HelloController.java
+++ src/main/java/com/example/springstudy/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.springstudy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 
-    @RequestMapping("/")
-    public String index(){
+    @RequestMapping("/{num}")
+    public String index(@PathVariable int num, Model model){
+        int res=0;
+        for(int i=0;i<=num;i++)
+            res+=i;
+        model.addAttribute("msg","total: "+res);
         return "index";
     }
 
