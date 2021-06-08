40c6d39964abb4ae532733147301529b80a9af5a
이승민 <sml7179@naver.com>
2021-06-08 11:31:31 +0900

    엔터티 create 사용

--- src/main/java/com/example/springstudy/controller/HelloController.java
+++ src/main/java/com/example/springstudy/controller/HelloController.java
@@ -4,9 +4,10 @@ import com.example.springstudy.MyData;
 import com.example.springstudy.repositories.MyDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import javax.annotation.PostConstruct;
 
 @Controller
 public class HelloController {
@@ -14,12 +15,44 @@ public class HelloController {
     @Autowired
     MyDataRepository repository;
 
-    @RequestMapping(value = "/")
-    public ModelAndView index(ModelAndView mv){
+    @PostConstruct
+    public void init(){
+        MyData md=new MyData();
+        md.setName("lee");
+        md.setAge(30);
+        md.setMail("sml@test.com");
+        md.setMemo("this is init Data");
+        repository.saveAndFlush(md);
+
+        MyData md2=new MyData();
+        md2.setName("kim");
+        md2.setAge(40);
+        md2.setMail("kim@test.com");
+        md2.setMemo("this is init Data2");
+        repository.saveAndFlush(md2);
+
+        MyData md3=new MyData();
+        md3.setName("park");
+        md3.setAge(20);
+        md3.setMail("park@test.com");
+        md3.setMemo("this is init Data3");
+        repository.saveAndFlush(md3);
+    }
+
+    @RequestMapping(value = "/", method=RequestMethod.GET)
+    public ModelAndView index(@ModelAttribute("formModel") MyData myData,ModelAndView mv){
         mv.setViewName("index");
-        Iterable<MyData> list=repository.findAll();
-        mv.addObject("data",list);
+        mv.addObject("msg","this is sample content.");
+        Iterable<MyData> list = repository.findAll();
+        mv.addObject("datalist",list);
         return mv;
+    }
+
+    @RequestMapping(value = "/", method=RequestMethod.POST)
+    @Transactional(readOnly = false)
+    public ModelAndView form(@ModelAttribute("formModel") MyData myData, ModelAndView mav){
+        repository.saveAndFlush(myData);
+        return new ModelAndView("redirect:/");
     }
 
     /*@RequestMapping(value="/", method = RequestMethod.POST)
