a08184a6bd308097d2e9d48f1e27987d771a2e4c
이승민 <sml7179@naver.com>
2021-06-11 13:14:53 +0900

    소스수정

--- src/main/java/com/example/springstudy/dao/MyDataDaoImpl.java
+++ src/main/java/com/example/springstudy/dao/MyDataDaoImpl.java
@@ -3,18 +3,14 @@ package com.example.springstudy.dao;
 import com.example.springstudy.MyData;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import javax.persistence.criteria.*;
 import java.util.List;
 
 public class MyDataDaoImpl implements MyDataDao<MyData> {
 
     private static final long serialVersionUID = 1L;
 
-    private EntityManager entityManager;
-
-    public MyDataDaoImpl() {
-        super();
-    }
+    private final EntityManager entityManager;
 
     public MyDataDaoImpl(EntityManager entityManager) {
         this.entityManager = entityManager;
@@ -22,10 +18,23 @@ public class MyDataDaoImpl implements MyDataDao<MyData> {
 
     @Override
     public List<MyData> getAll() {
-        Query query=entityManager.createQuery("select a from MyData a");
+        /*Query query=entityManager.createQuery("select a from MyData a");
         List<MyData> list=query.getResultList();
         entityManager.close();
+        return list;*/
+        int offset=1; //추출 위치 지정
+        int limit=1; //추출 개수 지정
+        List<MyData> list =null;
+        CriteriaBuilder builder=entityManager.getCriteriaBuilder();
+        CriteriaQuery<MyData> query=builder.createQuery(MyData.class);
+        Root<MyData> root=query.from(MyData.class);
+        query.select(root).orderBy(builder.asc(root.get("name")));
+        list = (List<MyData>)entityManager.createQuery(query)
+                //.setFirstResult(offset)
+                //.setMaxResults(limit)
+                .getResultList();
         return list;
+
     }
 
     @Override
@@ -40,14 +49,18 @@ public class MyDataDaoImpl implements MyDataDao<MyData> {
 
     @Override
     public List<MyData> find(String fstr) {
-        List<MyData> list =null;
+        /*List<MyData> list =null;
         String qstr="select a from MyData a where a.id = ?1 or a.name like ?2 or a.mail like ?3 ";
         Long fid=0L;
-        /*Query query=entityManager.createQuery(qstr).setParameter(1,fid)
-                .setParameter(2, "%"+fstr+"%")
-                .setParameter(3,fstr+"@%");*/
         Query query=entityManager.createNamedQuery("findWithName").setParameter("fname","%"+fstr+"%");
         list=query.getResultList();
+        return list;*/
+        CriteriaBuilder builder=entityManager.getCriteriaBuilder();
+        CriteriaQuery query=builder.createQuery(MyData.class);
+        Root<MyData> root=query.from(MyData.class);
+        query.select(root).where(builder.equal(root.get("name"),fstr));
+        List<MyData> list=null;
+        list=(List<MyData>)entityManager.createQuery(query).getResultList();
         return list;
     }
 
