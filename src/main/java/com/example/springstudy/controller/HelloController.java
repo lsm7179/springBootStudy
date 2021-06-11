e86981789b26de03342baa9537c01cfd9e73d2af
이승민 <sml7179@naver.com>
2021-06-11 09:51:06 +0900

    소스수정

--- src/main/java/com/example/springstudy/controller/HelloController.java
+++ src/main/java/com/example/springstudy/controller/HelloController.java
@@ -74,7 +74,7 @@ public class HelloController {
             mav.addObject("title","Find result");
             mav.addObject("msg",param+" 의 검색 결과");
             mav.addObject("value",param);
-            List<MyData> list =dao.cfind(param);
+            List<MyData> list =dao.find(param);
             mav.addObject("datalist",list);
         }
         return mav;
