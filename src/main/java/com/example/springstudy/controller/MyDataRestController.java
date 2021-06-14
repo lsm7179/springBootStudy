209d3832ca9f390fb77d6f3eb58fdfe20adff237
이승민 <sml7179@naver.com>
2021-06-14 09:43:45 +0900

    restController 작성

---
+++ src/main/java/com/example/springstudy/controller/MyDataRestController.java
@@ -0,0 +1,27 @@
+package com.example.springstudy.controller;
+
+import com.example.springstudy.data.MyData;
+import com.example.springstudy.service.MyDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MyDataRestController {
+
+    @Autowired
+    private MyDataService service;
+
+    @RequestMapping("/rest")
+    public List<MyData> restAll(){
+        return service.getAll();
+    }
+
+    @RequestMapping("/rest/{num}")
+    public MyData restBy(@PathVariable int num){
+        return service.get(num);
+    }
+}
